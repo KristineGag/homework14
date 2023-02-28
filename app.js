48e3cfc655df6d36b8e88eea3e51428f4966a332
KristineGag <christinegagosh@gmail.com>
2023-02-28 08:56:32 -0500

    task 2 and 3

--- app.js
+++ app.js
@@ -25,3 +25,39 @@ console.log("Number of elements in task 1 is: " + task1Length);
 let AverageOfTask1 = task1Sum / task1Length;
 
 console.log("average for task 1 elements is: " + AverageOfTask1);
+
+const task2objects = [
+  {
+    name: "ana",
+    age: 5,
+  },
+  {
+    name: "nino",
+    age: 7,
+  },
+  {
+    name: "mari",
+    age: 9,
+  },
+  {
+    name: "keso",
+    age: 11,
+  },
+  {
+    name: "kato",
+    age: 13,
+  },
+];
+
+console.table(task2objects);
+
+const task3Product = {
+  productTitle: "iphone 11",
+  productDescription: "new phone in black",
+  productPrice: 800,
+  shopAddresses: "London SW1",
+  productMemoryInGb: 256,
+  productResolution: "828p",
+};
+
+console.log(task3Product);
